ea24fb3078c3070901e8adb12da9e1ea402f8c7a
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-07-03 22:14:32 +0300

    add: db file to handle method accosiations and creations

---
+++ models/db.js
@@ -0,0 +1,54 @@
+const { Sequelize } = require("sequelize")
+const { DataTypes } = require("sequelize")
+const createBlog = require("./blog")
+const createCommentModel = require("./comment")
+const createLikeModel = require("./like")
+const createUser = require("./users")
+const createWritter = require("./writters")
+const sequelize = new Sequelize("blog_site", "root", "mysql254", {
+	host: "localhost",
+	dialect: "mysql",
+})
+const testConnection = async () => {
+	try {
+		await sequelize.authenticate()
+		console.log("Connection has been established successfully.")
+		return true
+	} catch (e) {
+		console.log("unable to establish connection:", e)
+		return false
+	}
+}
+const User = createUser(sequelize)
+const Writter = createWritter(sequelize)
+const Blog = createBlog(sequelize)
+const CommentModel = createCommentModel(sequelize)
+const LikeModel = createLikeModel(sequelize)
+
+User.hasOne(LikeModel)
+LikeModel.belongsTo(User)
+Blog.hasOne(LikeModel)
+LikeModel.belongsTo(Blog)
+
+User.hasOne(Writter, {
+	onDelete: "CASCADE",
+})
+Writter.belongsTo(User, {
+	foreignKey: {
+		allowNull: false,
+	},
+})
+
+Writter.hasOne(Blog, {
+	onDelete: "CASCADE", 
+	foreignKey: {
+		allowNull: false,
+	},
+})
+Blog.belongsTo(Writter)
+
+User.hasOne(CommentModel, {
+	onDelete: "CASCADE",
+})
+CommentModel.belongsTo(User)
+module.exports = { sequelize, User, Writter, Blog }
