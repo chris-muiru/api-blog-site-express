a61c721d7e56c0a468f75d327c53216c21eab871
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-07-08 19:21:25 +0300

    add: add blog crud functionality

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -4,16 +4,56 @@ const { Blog } = require("../models/db")
 router
 	.route("")
 	.get(async (req, res) => {
-		const blogs = await Blog.findAll()
-		res.json(blogs)
+		const getBlogs = await Blog.findAll()
+		res.json(getBlogs)
 	})
 	.post(async (req, res) => {
-		console.log(req.body)
 		try {
-			const blog = Blog.create(req.body)
-			res.status(200).json(blog)
+			const createBlog = await Blog.create(req.body)
+			res.status(200).json({ status: "blog created" })
 		} catch (e) {
-			throw e
+			if (e.name == "SequelizeUniqueConstraintError") {
+				res.status(403).json({ err: "user already created" })
+			} else {
+				res.status(500).json({ err: "an error occured" })
+			}
+		}
+	})
+router
+	.route("/:id")
+	.get(async (req, res) => {
+		let { id: blogId } = req.params
+		try {
+			const getBlog = await Blog.findAll({ where: { id: blogId } })
+			res.status(200).json(getBlog)
+		} catch (e) {
+			res.status(500).json({ err: "an error occured" })
+		}
+	})
+	.put(async (req, res) => {
+		let { id: blogId } = req.params
+		try {
+			const updateBlog = await Blog.update(req.body, {
+				where: {
+					id: blogId,
+				},
+			})
+			res.status(201).json({ message: "successfully created" })
+		} catch (e) {
+			res.status(404).json({ err: "invalid data" })
+		}
+	})
+	.delete(async (req, res) => {
+		let { id: blogId } = req.params
+		try {
+			const deleteBlog = await Blog.destroy({
+				where: {
+					id: blogId,
+				},
+			})
+			res.status(201).json({ message: "successfully deleted" })
+		} catch (e) {
+			res.status(500).json({ err: "internal server error" })
 		}
 	})
 module.exports = router
