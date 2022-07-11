a476773c6100b9c4ba3633241725dbe18f1deeed
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-07-11 21:45:05 +0300

    add: user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,3 +1,12 @@
-import express from "express"
+const express = require("express")
+const { User } = require("../models/db")
 const router = express.Router()
-router.route().get((req, res) => {})
+router.route("").post(async (req, res) => {
+	try {
+		let user = await User.create(req.body)
+		res.status(200).json(user)
+	} catch (err) {
+		res.status(500).json({ err: "internal server error" })
+	}
+})
+module.exports = router
