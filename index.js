856fd00e91ac228dd97ea6793269d42fc6ac4dc0
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-07-11 21:44:24 +0300

    add: like and user routes

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ const { sequelize } = require("./models/db")
 const { request } = require("express")
 const blogRoutes = require("./routes/blogRoutes.js")
 const commentRoutes = require("./routes/commentRoutes")
+const likeRoutes = require("./routes/likeRoutes")
+const userRoutes = require("./routes/userRoutes")
+// const session = require("express-session")
+// const SequelStore = require("sequelstore-connect")(session)
 sequelize.sync()
 
 dotenv.config()
+
 const app = express()
 const PORT = process.env.PORT || 8000
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use("/dash", blogRoutes)
 app.use("/comment", commentRoutes)
+app.use("/like", likeRoutes)
+app.use("/user", userRoutes)
 app.listen(PORT, () => {
 	console.log(`connected successfully on port ${PORT}`)
 })
