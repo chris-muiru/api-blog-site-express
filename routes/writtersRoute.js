35ec7a7c3ef49071538475a8f9c01008b70e5bde
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-10 04:46:31 +0300

    add: Writter component

---
+++ routes/writtersRoute.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const router = express.Router()
+const { Writter } = require("./../models/db")
+router
+	.route("")
+	.get(async (req, res) => {
+		try {
+			const writters = await Writter.findAll()
+			res.status(200).json(writters)
+		} catch (e) {}
+	})
+	.post(async (req, res) => {
+		// create a writter
+		const inputs = req.body
+		const loggedUser = req.user
+		try {
+			const writterExist = await Writter.findOne({
+				where: {
+					UserId: loggedUser,
+				},
+			})
+			if (!writterExist) {
+				const response = await Writter.create({
+					...inputs,
+					UserId: loggedUser,
+				})
+				res.status(200).json({ msg: "created user" })
+			} else {
+				res.status(200).json({ msg: "writter already exists" })
+			}
+		} catch (err) {
+			console.log(err)
+			res.status(504).json({ msg: "server error" })
+		}
+	})
+
+module.exports = router
