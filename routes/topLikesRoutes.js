9a182a207de3a571e46f8c36d9ca37348fc2c6d5
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-10 04:45:03 +0300

    add: sequelize query

--- routes/topLikesRoutes.js
+++ routes/topLikesRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const router = express.Router()
 const { LikeModel } = require("../models/db")
-router.get("", async(req, res) => {
-    blogs=await LikeModel.findAll({
-        
-    }) 
-
+router.get("", async (req, res) => {
+	try {
+		let likes = await LikeModel.findAll()
+		res.status(200).json(likes)
+	} catch (err) {
+		res.status(404).json({ err: "an error occured" })
+	}
 })
+module.exports = router
