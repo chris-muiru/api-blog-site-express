37302a1a773ff74996999054f61b1710103f134a
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-17 22:29:59 +0300

    add: create user functionality to create users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,9 +3,26 @@ const { User } = require("../models/db")
 const router = express.Router()
 router.route("").post(async (req, res) => {
 	try {
-		let user = await User.create(req.body)
-		res.status(200).json(user)
+		const { username, email, password, passwordConfirm } = req.body
+		console.log(req.body)
+		const userExist = await User.findOne({
+			where: {
+				email: email,
+			},
+		})
+		if (userExist) {
+			res.status(200).json({ msg: `user exists` })
+		} else {
+			if (password === passwordConfirm) {
+				let user = await User.create({ username, email, password })
+				res.status(200).json({ msg: `user created` })
+			} else {
+				console.log("nope")
+				res.status(200).json({ msg: "incorrect password" })
+			}
+		}
 	} catch (err) {
+		console.log(err)
 		res.status(500).json({ err: "internal server error" })
 	}
 })
